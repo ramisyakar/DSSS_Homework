5057d5767fada4f91929612b8a52c35e34fcb9c2
ramisyakar <69619371+ramisyakar@users.noreply.github.com>
2022-11-29 02:16:20 +0100

    setup, test and requirements pushed

---
+++ setup.py
@@ -0,0 +1,5 @@
+from distutils.core import setup
+from setuptools import find_packages
+
+setup(name="snowflake",version="0.1",author="DSSS_RamiscanYakar",author_email="ramiscan.yakar@fau.de",
+packages=find_packages(),install_requires=["numpy","turtles"])
